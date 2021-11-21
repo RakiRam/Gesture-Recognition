3d97e29daea43f10a22fdca08ff4e927681bb4e2
RakiRam <67856784+RakiRam@users.noreply.github.com>
2021-11-21 18:51:21 +0530

    Update main.py

--- micro_project/main.py
+++ micro_project/main.py
@@ -1,11 +1,9 @@
-
-#hello
 import time
 import os
 import cv2
-import HandTrackingModule as htm
+# import HandTrackingModule as htm
 
-from detector import handDetector
+import detector as d
 
 wCam, hCam = 640, 480
 
@@ -15,52 +13,50 @@ cap.set(4, hCam)
 
 folderPath = "fingerimgs"
 myList = os.listdir(folderPath)
-print(myList)
+# print(myList)
 overlayList = []
 for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
-    print(f'{folderPath}/{imPath}')
+    # print(f'{folderPath}/{imPath}')
     overlayList.append(image)
 
-print(len(overlayList))
+# print(len(overlayList))
 pTime = 0
 
-detector = handDetector(detectionCon=0.75)
+detector = d.handDetector(detectionCon=0.75)
 
-tipIds = []
+tipIds = [4, 8, 12, 16, 20]
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=True)
-    print(lmList)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
 
     if len(lmList) != 0:
         fingers = []
-
         # Thumb
-        if lmList:
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
 
         # 4 Fingers
-        for id in range(1, 4):
-            if lmList:
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
 
-        print(fingers)
+        # print(fingers)
         totalFingers = fingers.count(1)
-        print(totalFingers)
+        # print(totalFingers)
 
         h, w, c = overlayList[totalFingers - 1].shape
         img[0:h, 0:w] = overlayList[totalFingers - 1]
 
         cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
-                    10, (255, 0, 0), 25)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
